cc688ed0a776be2f7400fdd231d949ec92c7e918
kone <kone@kone.com>
2020-03-31 17:10:28 +0700

    generate fragment.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 window.onload = function (e) {
     this.document.title = "KoneCth"
-    this.setFavicon()
+    this.setFavicon();
+    this.fragmentLinks();
 }
 
 function setFavicon() {
@@ -8,4 +9,23 @@ function setFavicon() {
     link.rel = 'shortcut icon';
     link.href = "/images/favicon.png";
     document.head.appendChild(link);
+}
+
+function fragmentLinks() {
+    for (let i = 1; i < 6; i++) {
+        this.appendLink(document.getElementsByTagName('h' + i))
+    }
+}
+
+function appendLink(tag) {
+    for (let i = 0; i < tag.length; i++) {
+        item = tag[i]
+        if (!item.id) {
+            continue;
+        }
+        let a = document.createElement("a")
+        a.innerText="🔗";
+        a.setAttribute('href',window.location.href.split('#')[0] + "#" +item.id);
+        item.appendChild(a)
+    }
 }
